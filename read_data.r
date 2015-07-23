933b21723f39bc1bbd9dfedaee99d79858099379
miltonlab <1000ton.lab@gmail.com>
2015-07-23 17:56:03 -0500

    finish html method

---
+++ read_data.r
@@ -0,0 +1,24 @@
+setwd("~/researchlab-usability.edu.ec")
+
+readHtml <- function (name, file) {
+  library('XML')
+  name = 'UTPL'
+  file = 'data/Universidad Tecnica Particular de Loja - Robert Figueroa.htm'
+  library('XML')
+  html <- readHTMLTable(file)
+  table <- na.omit(data.frame(html[2]))
+  table[, 6] <- sapply(
+    table[,6], 
+    function(x) as.numeric(gsub('%', '',x))
+    )
+  table <- table[, c(1,6)]
+  rownames(table) <- table[,1]
+  table[,1] <- NULL
+  table <- t(table)
+  rownames(table)<-c(name)
+  table
+}
+
+readExcel <- function(name, file) {
+  
+}
